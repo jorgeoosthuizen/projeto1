218ece96d8d19ac318edd531c5dcc3bba00503f9
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-05-09 11:51:04 +0100

    new folder + home cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,46 @@
 <template>
-    <div class="home">
-      <h1>This is an home page</h1>
+  <div class="home">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <div class="card h-100 border-0">
+          <img src="" class="card-img-top" alt="..." />
+          <img src="" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card h-100 border-0">
+          <img src="" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card h-100 border-0">
+          <img src="" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
