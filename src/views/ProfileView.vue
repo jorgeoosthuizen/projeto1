ebfa91d9ed19d6a6b97e8c85c300b3e95381a437
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-06-03 17:12:59 +0100

    Revert "a"
    This reverts commit a47a74c67289c85421628c05317b249bd5e7b665.

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,83 +1,106 @@
 <template>
   <div class="container-fluid mt-4">
-    <button @click="getAllUsers" class="btn btn-primary">Get All Users</button>
-    <div v-if="User.length > 0" class="Users">
-      <div v-for="user in User" :key="user.id" class="user">
-        <p>ID: {{ user.id }}</p>
-        <p>Email: {{ user.email }}</p>
-        <p>Created At: {{ user.createdAt }}</p>
+    <div class="row">
+      
+      <div class="col-md-2 px-5"> 
+        <div class="profile-image-container rounded-circle border border-primary"></div>
       </div>
-    </div>
-    <div v-else>
-      <p>No User Data</p>
-    </div>
-    <div v-if="error">
-      <p>{{ error }}</p>
+      
+      <div class="col-md-5">
+        <div class="middle-column">
+          <div class="row">
+            <div class="col-3">
+              <h3>Your team:</h3> 
+            </div>
+          </div>
+          <div class="row row-cols-md-6 g-1 "> 
+            <div v-for="favorite in favorites" :key="favorite.pokemonName" class="col">
+              <div class="card custom-card mb-4">
+                <img :src="favorite.pokemonArtwork" class="card-img-top img-fluid" alt="Pokemon Image" />
+                <div class="card-body text-center p-2">
+                  <h6 class="card-title mb-0">{{ favorite.pokemonName }}</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h6>You can change your team by removing/adding favorites in the pokedex search!</h6>
+      </div>
     </div>
   </div>
 </template>
 
+
+
+
 <script setup>
-import { ref } from 'vue';
-import axios from 'axios';
+import { useAuthStore } from "../store/auth"; 
+import { ref,  onMounted } from 'vue';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import db from '../firebase/firebase';
 
-const User = ref([]);
-const error = ref('');
 
-const getAllUsers = async () => {
+const favorites = ref([]);
+
+
+
+onMounted(async () => {
+  favorites.value = await getFavorites();
+});
+
+const getFavorites = async () => {
+  const authStore = useAuthStore();
+  const favorites = ref([]);
+
   try {
-    const response = await axios.get('http://localhost:3000/users', {
-      headers: {
-        'If-Modified-Since': 'Mon, 26 Jul 1997 05:00:00 GMT'
-      }
+    const userId = authStore.user ? authStore.user.uid : null; // Get current user ID
+
+    if (!userId) {
+      throw new Error('No authenticated user found.');
+    }
+
+    // Query for current user's favorites
+    const q = query(collection(db, 'favorites'), where('user_id', '==', userId));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const favorite = {
+        pokemonName: data.pokemon_name,
+        pokemonArtwork: data.pokemon__artwork
+      };
+      favorites.value.push(favorite); 
     });
-
-    const data = response.data;
-
-    User.value = data.map(user => ({
-      id: user.user_id,
-      email: user.email,
-      createdAt: user.createdAt
-    }));
-
-  } catch (err) {
-    error.value = 'Falha ao carregar User';
+  } catch (error) {
+    console.error('Error getting favorites:', error.message);
   }
+
+  return favorites.value; 
 };
+
 </script>
 
 <style scoped>
-.container-fluid {
-  padding: 20px;
-}
-
-.Users {
-  margin-top: 20px;
-}
-
-.user {
-  margin-bottom: 15px;
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-}
 
 .custom-card {
-  width: 100px;
+  width: 100px; 
 }
 
 .custom-card .card-img-top {
-  width: 50px;
-  height: 50px;
-  margin: 0 auto;
+  width: 50px; 
+  height: 50px; 
+  margin: 0 auto; 
 }
 
+
+
 .profile-image-container {
-  width: 200px;
-  height: 200px;
+  width: 200px; 
+  height: 200px; 
   background-image: url(../assets/icons/pokemon-trainer.png);
-  background-size: contain;
+  background-size: contain; 
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: center; 
 }
+
 </style>
